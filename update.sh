3c33fe268ce1020d6891ff4984d89ea7466c5514
Kevin Coursey <kevinscoursey@gmail.com>
2021-12-29 14:18:47 -0800

    .py permissions

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 
 echo Updating ./Bakingthenumbers directory from https://github.com/Hamderber/Bakingthenumbers repository
 sudo git pull https://github.com/Hamderber/Bakingthenumbers
-sudo chmod +x *.py
-echo Gave execution permission on all .py files
+sudo chmod 705 *.py
+echo Gave permissions on all .py files
