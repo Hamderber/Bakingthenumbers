7bfc2250926e8713152be2fa5a94db89005852ae
Kevin Coursey <kevinscoursey@gmail.com>
2021-12-29 13:25:13 -0800

    Python

---
+++ index.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env puthon
+
+#Turn on debug mode
+
+import cgitb
+cgitb.enable()
+
+#Print required headers
+print("Content-Type:text/html;charset=utf-8")
+print()
+print("Hello World")
